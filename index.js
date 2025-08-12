3c045b3e035fb68a5e6a626bcb74b665325632f7
Kanakm1502 <kanakm1502@gmail.com>
2025-08-12 23:25:35 +0530

    added account locking functionality

--- index.js
+++ index.js
@@ -30,34 +30,27 @@ app.post("/api/register", async (req, res) => {
   //Encrypting the password  
   const saltRounds = 10;
 
-  const salt = await bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) {
-      console.log("Error in salt generation.");
-      return;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    //Checking if user is new or not, if not registering
+    const result = await db.query(
+      "SELECT uid FROM user_data WHERE uid = $1",
+      [username]
+    );
+
+    if (result.rows.length === 0) {
+
+      await db.query("INSERT INTO user_data (uid , pwd) VALUES ($1 , $2)", [username, hashedPassword]);
+      res.sendStatus(200);
+    } else {
+      console.log("User already exists.")
+      res.sendStatus(200);
     }
-  });
-
-  const hash = await bcrypt.hash(password, salt, (err, hash) => {
-    if (err) {
-      console.log("Error in hashing password!")
-      return;
-    }
-  });
-
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  //Checking if user is new or not, if not registering
-  const result = await db.query(
-    "SELECT uid FROM user_data WHERE uid = $1",
-    [username]
-  );
-
-  if (result.rows.length === 0) {
-
-    await db.query("INSERT INTO user_data (UID , pwd) VALUES ($1 , $2)", [username, hashedPassword]);
-    res.sendStatus(200);
-  } else {
-    console.log("User already exists.")
+  }
+  catch (err) {
+    console.error("Error registering user:", err);
+    res.sendStatus(500);
   }
 });
 
@@ -68,44 +61,69 @@ app.post("/api/login", async (req, res) => {
   const password = req.body["password"];
 
   //Check if user is registered.
-  const users = await db.query(
-    "SELECT * FROM user_data WHERE uid = $1",
-    [username]
-  );
+  try {
+    const users = await db.query(
+      "SELECT * FROM user_data WHERE uid = $1",
+      [username]
+    );
 
-  if (users.rows.length !== 0) {
-    const data = users.rows[0];
-    const username = data.uid;
-    const storedHashedPassword = data.pwd;
-    const last_login = data.last_login;
+    if (users.rows.length !== 0) {
+      const data = users.rows[0];
+      const username = data.uid;
+      const storedHashedPassword = data.pwd;
+      const status = data.status;
 
-    //Compare the hashed password with the user enterd password
-    const result = await bcrypt.compare(password, storedHashedPassword, (err, result) => {
-      if (err) {
-        console.error('Error comparing passwords:', err);
-        return;
-      }
+      //Compare the hashed password with the user enterd password
+      const result = await bcrypt.compare(password, storedHashedPassword)
 
+      const date = new Date();
+
+      // If password is correct:
       if (result) {
-        console.log('Passwords match! User authenticated.');
-        const date = new Date();
-        var hours = Math.abs(date - last_) / 36e5;
-        if(!last_login){
-          console.log(date-last_login);
-          db.query("UPDATE user_data SET last_login = $1 WHERE uid = $2",[date,username])
+        if (status == "open") {
+          console.log('Passwords match! User authenticated.');
+        }
+        else {
+          // Checking if account lock has exceeded 24 hours
+          const time_served_result = await db.query("SELECT count(uid) AS ts FROM user_data WHERE uid = $1 AND $2 - lock_time > INTERVAL '24 hours'",[username,date]);
+          const time_served = parseInt(time_served_result.rows[0].ts, 10);
+          // If yes -> open the account and authenticate the user.
+          if(time_served == 1){
+            await db.query("UPDATE user_data SET status = 'open' WHERE uid = $1", [username]);
+            console.log('Passwords match! User authenticated.');
+          }else{
+            console.log("ACCOUNT IS LOCKED TRY AGAIN IN 24 HRS!")
+          }
         }
       } else {
-        console.log('Passwords do not match! Authentication failed.');
+        // Logging the user login activity
+        if (status == "open") {
+          console.log('Passwords do not match! Authentication failed.');
+          await db.query("INSERT INTO user_logs (login_time,uid) VALUES ($1,$2)", [date, username]);
+        }
+        // Getting the no. of failed attempts in the last 12 hrs.
+        const failed_result = await db.query("SELECT COUNT(logid) AS fail_count FROM public.user_logs WHERE uid = $1 AND $2 - login_time < INTERVAL '12 hours' GROUP BY uid", [username, date]);
+        const failed_count = parseInt(failed_result.rows[0].fail_count, 10)
+        if (failed_count == 5) {
+          console.log("ACCOUNT BLOCKED, TRY AGAIN IN 24 HRS!!")
+          await db.query("UPDATE user_data SET status = 'locked' WHERE uid = $1", [username]);
+          await db.query("UPDATE user_data SET lock_time = $1 WHERE uid = $2", [date,username]);
+        } else {
+          console.log("Failed Attempts: ", failed_count);
+        }
       }
-    });
-  } else {
-    console.log("User not found!")
+
+    } else {
+      console.log("User not found!")
+    }
+    res.sendStatus(200);
   }
-  res.sendStatus(200);
+  catch (err) {
+    console.error("Error during login:", err);
+    res.sendStatus(500);
+  }
+
 });
-
-//TIMEOUT 
-
 
 
 app.listen(port, () => {
